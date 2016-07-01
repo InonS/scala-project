550cfbc77fe059a051b544e440bc0535be43bb42
Inon Sharony <InonShar@TAU.ac.IL>
2016-07-01 23:18:32 +0200

    Mark Lewis IPPSUS: Option and Parametric Functions

---
+++ src/scala5marklewis/scala51ippsus/Scala11Option.scala
@@ -0,0 +1,39 @@
+package scala5marklewis.scala51ippsus
+
+/**
+  * scala-project
+  * Created by admin on 2016-07-01.
+  */
+object Scala11Option {
+
+  def main(args: Array[String]) {
+    val lst = List(1, 2, 3, 4)
+
+    findGt(lst, 5)
+    findGt(lst, 3)
+  }
+
+  private def findGt(lst: List[Int], n: Int): Unit = {
+    val gtn = lst find (_ > n)
+    println(gtn)
+    println(gtn isEmpty)
+    println(gtn nonEmpty)
+
+    println(gtn getOrElse 0)
+
+    try {
+      println(gtn get)
+    } catch {
+      case e: NoSuchElementException => println(n)
+    }
+
+    gtn match {
+      case Some(x) => println(x) // implicit parameter not supported?!
+      case None => println(n)
+    }
+
+    println(gtn map (_ * 2)) // a None is mapped to a None
+
+    println(gtn find (_ % 2 == 0))
+  }
+}
---
+++ src/scala5marklewis/scala51ippsus/Scala12ParametricFunctions.scala
@@ -0,0 +1,39 @@
+package scala5marklewis.scala51ippsus
+
+/**
+  * scala-project
+  * Created by admin on 2016-07-01.
+  */
+object Scala12ParametricFunctions {
+
+  def identity[T](t: T): T = t
+
+  def main(args: Array[String]) {
+    println(identity(1))
+    println(identity(1.0))
+    println(identity("a"))
+
+    println(ourFold(List(1, 2, 3, 4, 5), 0)(_ + _))
+    println(ourFold(List(1, 2, 3, 4, 5), "")(_ + _))
+  }
+
+  /**
+    *
+    * first parameter list:
+    *
+    * @param lst
+    * @param base
+    *
+    * second parameter list:
+    * @param f
+    * @tparam A
+    * @tparam B
+    * @return
+    */
+  def ourFold[A, B](lst: List[A], base: B)(f: (A, B) => B): B = lst match {
+    case Nil => base
+    case h :: t => f(h, ourFold(t, base)(f))
+  }
+
+}
+

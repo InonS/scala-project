49a9d13b8fbf06ac929befe044443d79dfa1cc72
Inon Sharony <InonShar@TAU.ac.IL>
2016-08-15 00:01:14 +0200

    Mark Lewis IPPSUS: Yield keyword and indices method

---
+++ src/scala5marklewis/scala51ippsus/Scala13Yield.scala
@@ -0,0 +1,36 @@
+package scala5marklewis.scala51ippsus
+
+/**
+  * scala-project
+  * Created by admin on 2016-08-07.
+  */
+object Scala13Yield {
+  def main(args: Array[String]): Unit = {
+    // 3x^2 + 2x - 5
+    val coeffs = Array(3.0, 2.0, -5.0)
+    val x = 1
+
+    print(evalPolyRange(coeffs, x))
+    print(evalPolyYield(coeffs, x))
+  }
+
+  def evalPolyRange(coeffs: Array[Double], x: Double): Double = {
+    var sum = 0.0
+    var power = 1.0 // math.pow(x, 0)
+    for (i <- coeffs indices) {
+      sum += coeffs(i) * power // this iteration's 'power' was calculated on previous iteration
+      power *= x // math.pow(x, i) for next iteration
+    }
+    sum
+  }
+
+  def evalPolyYield(coeffs: Array[Double], x: Double): Double = {
+    yieldMonomialEval(coeffs, x).sum
+  }
+
+  private def yieldMonomialEval(coeffs: Array[Double], x: Double): IndexedSeq[Double] = {
+    for (i <- coeffs indices) yield {
+      coeffs(i) * math.pow(x, coeffs.length - 1 - i)
+    }
+  }
+}

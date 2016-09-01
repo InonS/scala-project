e99ac53625074ae2be8de5a0b7a55f220bc9c20b
Inon Sharony <InonShar@TAU.ac.IL>
2016-09-01 23:46:43 +0200

    Mark Lewis IPPSUS: 'for' loop patterns, variables, and comprehensions

---
+++ src/scala5marklewis/scala51ippsus/Scala14PatternsInLoops.scala
@@ -0,0 +1,38 @@
+package scala5marklewis.scala51ippsus
+
+/**
+  * scala-project
+  * Created by admin on 2016-09-01.
+  */
+object Scala14PatternsInLoops {
+  def main(args: Array[String]): Unit = {
+    var tuples = List[(Int, Int)]()
+    for (i <- 1 to 5) {
+      tuples = (i, i + 1) :: tuples
+    }
+
+    for ((n1, n2) <- tuples) println("(" + n1 + ',' + n2 + ')')
+
+    // within 'for' loop, unmatched cases are just skipped (matching doesn't have to be exhaustive)
+    for ((1, n2) <- tuples) println("(" + 1 + ',' + n2 + ')')
+    for ((n1, n2) <- tuples if n1 % 2 == 0) println("(" + n1 + ',' + n2 + ')')
+    // loop variables, too!
+    for ((n1, n2) <- tuples; q = n1 % 2; if q == 0) println(q + ": (" + n1 + ',' + n2 + ')')
+    // same thing, multiline 'for' block
+    for {
+      (n1, n2) <- tuples
+      q = n1 % 2
+      if q == 0
+    } println(q + ": (" + n1 + ',' + n2 + ')')
+
+    println(splitMapMatch("1,2,3"))
+  }
+
+  /**
+    * If you know in advance the exact type you're parsing, you can use non-exhaustive pattern matching as well
+    */
+  def splitMapMatch(tripleFiledCsv: String): (Int, Int, Int) = {
+    val Array(a, b, c) = tripleFiledCsv.split(",").map(_.toInt)
+    (a, b, c)
+  }
+}
---
+++ src/scala5marklewis/scala51ippsus/Scala15Multidimensions.scala
@@ -0,0 +1,30 @@
+package scala5marklewis.scala51ippsus
+
+/**
+  * scala-project
+  * Created by admin on 2016-09-01.
+  */
+object Scala15Multidimensions {
+  def main(args: Array[String]): Unit = {
+    println(multiplicationFactors)
+    println(multiplicationTable)
+  }
+
+  /**
+    * 'for' comprehended as 'flatMap'
+    */
+  def multiplicationFactors = {
+    for (x <- 1 to 10; y <- 1 to 10) yield x * y
+  }
+
+  /**
+    * 'for' comprehended as chained 'map's
+    */
+  def multiplicationTable = {
+    for (x <- 1 to 10) yield {
+      for (y <- 1 to 10) yield {
+        x * y
+      }
+    }
+  }
+}

f7f77261cd79eab04f9019c5c8bdebfa708c5da5
Inon Sharony <InonShar@TAU.ac.IL>
2016-10-30 23:23:16 +0200

    Mark Lewis IAPUS: Deprecated in favor of (part 1) IPPSUS & (part 2) OOADSUS

---
+++ src/scala5marklewis/scala52iapus/package.scala
@@ -0,0 +1,11 @@
+package scala5marklewis
+
+/**
+  * scala-project
+  * Created by admin on 2016-10-30.
+  *
+  * @deprecated See constituent packages for deprecation details.
+  */
+package object scala52iapus {
+
+}
---
+++ src/scala5marklewis/scala52iapus/part2/package.scala
@@ -0,0 +1,11 @@
+package scala5marklewis.scala52iapus
+
+/**
+  * scala-project
+  * Created by admin on 2016-10-30.
+  *
+  * @deprecated [[scala5marklewis.scala53ooadsus]]
+  */
+package object part2 {
+
+}

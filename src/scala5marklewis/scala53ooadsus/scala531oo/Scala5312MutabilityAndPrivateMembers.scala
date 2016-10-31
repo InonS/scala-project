5e5f432c9a692b869501a93676ee8fb46ab7a233
Inon Sharony <InonShar@TAU.ac.IL>
2016-11-01 00:09:59 +0200

    Mark Lewis OOADSUS: Scala5312MutabilityAndPrivateMembers & Scala5313PublicAccessorsAndValidityChecks

---
+++ src/scala5marklewis/scala53ooadsus/scala531oo/Scala5312MutabilityAndPrivateMembers.scala
@@ -0,0 +1,60 @@
+package scala5marklewis.scala53ooadsus.scala531oo
+
+/**
+  * scala-project
+  * Created by admin on 2016-10-31.
+  */
+object Scala5312MutabilityAndPrivateMembers {
+
+  /**
+    * @param x immutable property
+    */
+  class ImmutableObject(val x: List[Int] = Nil) {
+    /**
+      * Mutating an immutable object is not possible, so any mutators must return a new [[ImmutableObject]]
+      *
+      * @param y Int to cons onto existing list [[x]]
+      * @return new [[ImmutableObject]] with updated [[x]]
+      */
+    def :+(y: Int): ImmutableObject = new ImmutableObject(y :: x)
+
+  }
+
+  /**
+    * @param _f mutable property (Arrays are mutable objects in themselves, too), so private
+    */
+  class MutableForce(private var _f: List[Int]) {
+    /**
+      * Since [[_f]] is mutable, we perform the cons in-place.
+      *
+      * @param g element to cons onto existing [[_f]]
+      */
+    def :+(g: Int) {
+      _f ::= g
+    }
+
+    /** public accessor
+      * Since [[_f]] is a [[List]] (an immutable type), it's safe to return a reference to it.
+      * had it been a mutable type (e.g. [[Array]]) we would allow access only via a defensive copy.
+      */
+    def f = _f
+
+    /** property assignment method */
+    def f_=(new_f: List[Int]) = f = new_f
+
+    /**
+      * unary, prefix operator
+      * private method, just for fun
+      */
+    private def unary_++() {
+      this :+ f.head
+      this f_= f
+    }
+  }
+
+  def main(args: Array[String]) {
+
+  }
+
+
+}
---
+++ src/scala5marklewis/scala53ooadsus/scala531oo/Scala5313PublicAccessorsAndValidityChecks.scala
@@ -0,0 +1,36 @@
+package scala5marklewis.scala53ooadsus.scala531oo
+
+import scala.collection.immutable.NumericRange
+import scala5marklewis.scala53ooadsus.scala531oo.Scala5312MutabilityAndPrivateMembers.ImmutableObject
+
+/**
+  * scala-project
+  * Created by admin on 2016-10-31.
+  */
+object Scala5313PublicAccessorsAndValidityChecks {
+
+  class ValidatedImmuableObject() extends ImmutableObject {
+
+    private val range = NumericRange.apply(1, 3, 1)
+
+    private def validate(x: Int): Boolean = {
+      require(range contains x)
+      true
+    }
+
+    /** what a silly, symbolic name! */
+    def ?=(x: Int): Option[Int] = if (!validate(x)) None else Some(x)
+
+    /**
+      * Mutating an immutable object is not possible, so any mutators must return a new [[ImmutableObject]]
+      *
+      * @param y Int to cons onto existing list [[x]]
+      * @return new [[ImmutableObject]] with updated [[x]]
+      */
+    override def :+(y: Int): ImmutableObject = if (!validate(y)) this else super.:+(y)
+
+    /** what a silly, symbolic name! */
+    def ?+=(y: Int): Option[ImmutableObject] = if (!validate(y)) None else Some(super.:+(y))
+  }
+
+}
